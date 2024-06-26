85dcec1a27f088da7ebbe4f9d92da3ce2d93baff
gerardoGP <gerardo210299@gmail.com>
2024-06-26 15:25:46 -0500

    v1

---
+++ database/migrations/2024_06_26_160845_create_works_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_tit');
+            $table->string('company_name');
+            $table->date('w_start')->nullable();
+            $table->date('w_end')->nullable();
+            $table->string('url_drive')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title')Bienvenido a mi Portafolio@endsection
+
+@section('content')
+
+@endsection
